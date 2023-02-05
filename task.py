735f7c423ea24082a17b1665f44bb16a4e4ae9f1
Nick Ahn <n_ahn2@u.pacific.edu>
2023-02-04 23:13:00 -0800

    Add task class

---
+++ task.py
@@ -0,0 +1,25 @@
+import json
+'''
+Task {
+    "Text":task description
+    "Priority": task priority
+    "Complete": False
+    "
+}
+'''
+class Task:
+    def __init__(self, text="", priority=4) -> None:
+        self.text = text
+        self.priority = priority
+        self.complete = False
+    
+    '''
+    Return class attributes as a dictionary in json format
+    '''
+    def to_json(self) -> dict:
+        dic = {
+            "Text": self.text,
+            "Priority": self.priority,
+            "Complete": self.complete
+        }
+        return dic
